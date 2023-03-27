9c071f7a17edd5eba1bb7ccbaf322c333941aa63
Tuan Tran <anhtuanbw@gmail.com>
2023-03-27 22:23:00 +0700

    Revert "update native ads"
    This reverts commit bc760d0ac0046f5a6287a2dfdf05b6607c797fb6.

--- src/pages/native-ad-1.js
+++ src/pages/native-ad-1.js
@@ -28,11 +28,11 @@ export default function Home() {
         const sdk = window.AiactivSDK || {};
         const adUnits = [
           {
-            inventoryId: 411,
+            inventoryId: 523,
             placementId: 'display_ads_1',
             options: {
               native: {
-                template_id: 'template-1',
+                template_id: 'template-2',
               },
             },
           },
